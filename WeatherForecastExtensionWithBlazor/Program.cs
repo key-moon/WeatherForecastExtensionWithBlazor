7757eeeb7fd7adb399654b41cec03d764bd410a2
keymoon <kymn0116@gmail.com>
2019-12-07 20:21:15 +0900

    init project

---
+++ WeatherForecastExtensionWithBlazor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace WeatherForecastExtensionWithBlazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
