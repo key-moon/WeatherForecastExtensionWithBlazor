6414f285e64d2a5ca1647a7704628a2f7300bf5e
keymoon <kymn0116@gmail.com>
2019-12-07 22:19:59 +0900

    add forecast model

---
+++ WeatherForecastExtensionWithBlazor/Model/Forecast.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WeatherForecastExtensionWithBlazor.Model
+{
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    public class Rain
+    {
+        [JsonPropertyName("3h")]
+        public double threeh { get; set; }
+    }
+
+    public class Snow
+    {
+        [JsonPropertyName("3h")]
+        public double threeh { get; set; }
+    }
+
+    public class Sys
+    {
+        public string pod { get; set; }
+    }
+
+    public class Forecast
+    {
+        public int dt { get; set; }
+        public Main main { get; set; }
+        public Weather[] weather { get; set; }
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public Rain snow { get; set; }
+        public Sys sys { get; set; }
+        public string dt_txt { get; set; }
+    }
+
+    public class Coord
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Forecasts
+    {
+        public string cod { get; set; }
+        public int message { get; set; }
+        public int cnt { get; set; }
+        public Forecast[] list { get; set; }
+        public City city { get; set; }
+    }
+}
