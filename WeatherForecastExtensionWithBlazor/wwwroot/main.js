2bde53cbe3621ec49a1d4ce1b9389aa651e2714f
keymoon <kymn0116@gmail.com>
2019-12-07 22:25:26 +0900

    add front js

---
+++ WeatherForecastExtensionWithBlazor/wwwroot/main.js
@@ -0,0 +1,8 @@
+﻿async function get_coord() {
+    return new Promise((success, reject) => {
+        if (!navigator.geolocation) reject("Geolocation is not supported by this browser.");
+        navigator.geolocation.getCurrentPosition((position) => {
+            success({ lat: position.coords.latitude, lon: position.coords.longitude });
+        });
+    }); 
+}
